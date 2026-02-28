4db87135371decaa4bf6409c1713d3419688afeb
abyssiniatech <surafelmengist2015@gmail.com>
2026-02-28 17:25:42 +0300

    fix errors

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Homepage() {
   return (
-    <div>
-      welcome
-      
+    <div className="bg-indigo-900 text-white min-h-screen flex justify-center items-center">
+      <p>Home page!</p>
+
     </div>
   );
 }
