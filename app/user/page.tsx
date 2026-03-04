e373956533a13c719171436482e313fd60070b40
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-04 20:14:17 +0300

    add mini input pro

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,8 +1,10 @@
 import Counter1 from "../components/Counter1"
+import Name from "../components/Name";
 const User=()=>{
     return(
-        <div className="min-h-screen flex justify-center items-center  bg-indigo-900 text-white">
+        <div className="min-h-screen flex flex-col justify-center items-center  bg-indigo-900 text-white">
             <Counter1 />
+            <Name />
         </div>
     )
 }
