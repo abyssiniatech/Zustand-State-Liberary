0abb62218ebd597eab77e948c60e9121fc142549
abyssiniatech <surafelmengist2015@gmail.com>
2026-03-04 19:54:37 +0300

    add increment

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,10 @@
-export default function Contact(){
-    return(
-        <div className="min-h-screen flex justify-center items-center  bg-indigo-900 text-white">
-            <h1>Contact page!</h1>
-        </div>
-    )
+// Make sure the filename and path are correct. If your file is named 'CounterApp.tsx', update the import:
+import CounterApp from "../components/CounterApp";
+export default function Contact() {
+  return (
+    <div className="min-h-screen flex justify-center items-center  bg-indigo-900 text-white">
+      <h1>Contact page!</h1>
+      <CounterApp />
+    </div>
+  );
 }
